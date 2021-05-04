5b0582316194306d1891ffc1385f28ba65f4f125
robertzhangwenjie <robertzhangwenjie@gmail.com>
2021-05-04 22:14:17 +0800

    init commit

---
+++ user/config/settings.py
@@ -0,0 +1,27 @@
+import logging
+
+from playhouse.pool import PooledMySQLDatabase
+from playhouse.shortcuts import ReconnectMixin
+
+class ReconnectMysqlDatabase(ReconnectMixin,PooledMySQLDatabase):
+    '''
+    使用ReconnectMixin 防止数据库连接断开
+    使用PooledMySQLDatabase 使用数据库连接池
+    '''
+    pass
+
+MYSQL_CONFIG= {
+    "host": "www.robertzwj.com",
+    "port":3306,
+    "database":"go_user",
+    "user":"robert",
+    "password":"123456"
+}
+
+db = ReconnectMysqlDatabase(**MYSQL_CONFIG)
+
+logger = logging.getLogger("peewee")
+logger.setLevel(logging.DEBUG)
+
+logger.addHandler(logging.StreamHandler())
+
